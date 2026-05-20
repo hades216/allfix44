edd567fa1185abd6d1efba4a7e697ebf103b2ece
hades216 <hades216@users.noreply.github.com>
2026-05-21 00:23:17 +0500

    fix-netlify-build

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,10 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import netlifyPlugin from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
-  build: {
-    outDir: "dist/public",
-    emptyOutDir: true,
-  },
+  cloudflare: false,
   tanstackStart: {
     server: { entry: "server" },
   },
-  plugins: [
-    netlifyPlugin({
-      edge: false,
-      functions: "dist/functions",
-      publish: "dist/public",
-    }),
-  ],
+  plugins: [netlifyPlugin()],
 });
+
